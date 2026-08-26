031b5971fcf3a6d9305672b59a5cd257db77dde7
Waleed Zidan <83763887+waleedzidan2020@users.noreply.github.com>
2026-08-26 12:01:28 +0300

    Implement efficient daily cleanup service

---
+++ Services/AdminCleanupService.cs
@@ -0,0 +1,56 @@
+using AttendanceSystem.Data;
+using AttendanceSystem.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendanceSystem.Services;
+
+public class AdminCleanupService : IAdminCleanupService
+{
+    private readonly ApplicationDbContext _db;
+
+    public AdminCleanupService(ApplicationDbContext db) => _db = db;
+
+    public async Task<AdminCleanupResponse> DeleteTodayAsync(CancellationToken ct = default)
+    {
+        var egyptTimeZone = GetEgyptTimeZone();
+        var egyptNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, egyptTimeZone);
+        var startLocal = DateTime.SpecifyKind(egyptNow.Date, DateTimeKind.Unspecified);
+        var endLocal = startLocal.AddDays(1);
+        var startUtc = TimeZoneInfo.ConvertTimeToUtc(startLocal, egyptTimeZone);
+        var endUtc = TimeZoneInfo.ConvertTimeToUtc(endLocal, egyptTimeZone);
+
+        await using var transaction = await _db.Database.BeginTransactionAsync(ct);
+
+        try
+        {
+            var deletedRejectedAttempts = await _db.AttendanceAttempts
+                .Where(x => !x.Accepted && x.AttemptTimeUtc >= startUtc && x.AttemptTimeUtc < endUtc)
+                .ExecuteDeleteAsync(ct);
+
+            var deletedAttendance = await _db.AttendanceRecords
+                .Where(x => x.CheckInTimeUtc >= startUtc && x.CheckInTimeUtc < endUtc)
+                .ExecuteDeleteAsync(ct);
+
+            await transaction.CommitAsync(ct);
+
+            return new AdminCleanupResponse(deletedAttendance, deletedRejectedAttempts);
+        }
+        catch
+        {
+            await transaction.RollbackAsync(ct);
+            throw;
+        }
+    }
+
+    private static TimeZoneInfo GetEgyptTimeZone()
+    {
+        try
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById("Africa/Cairo");
+        }
+        catch (TimeZoneNotFoundException)
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById("Egypt Standard Time");
+        }
+    }
+}
