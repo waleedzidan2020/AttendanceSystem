1300d87c27ba42e144182d961d2cb2a79bbfdfec
Waleed Zidan <83763887+waleedzidan2020@users.noreply.github.com>
2026-08-27 07:45:59 +0300

    Make admin cleanup date-aware

--- Services/IAdminCleanupService.cs
+++ Services/IAdminCleanupService.cs
@@ -4,5 +4,6 @@ namespace AttendanceSystem.Services;
 
 public interface IAdminCleanupService
 {
+    Task<AdminCleanupResponse> DeleteByDateAsync(DateOnly date, CancellationToken ct = default);
     Task<AdminCleanupResponse> DeleteTodayAsync(CancellationToken ct = default);
 }
