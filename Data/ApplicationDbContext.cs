3f6f8d49451c306746d73876abba745dfd5b7b80
Waleed Zidan <83763887+waleedzidan2020@users.noreply.github.com>
2026-08-29 17:57:30 +0300

    Map WebAuthn device verification data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,6 +13,11 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
     public DbSet<WorkSite> WorkSites => Set<WorkSite>();
     public DbSet<AttendanceRecord> AttendanceRecords => Set<AttendanceRecord>();
     public DbSet<AttendanceAttempt> AttendanceAttempts => Set<AttendanceAttempt>();
+    public DbSet<EmployeeWebAuthnCredential> EmployeeWebAuthnCredentials => Set<EmployeeWebAuthnCredential>();
+    public DbSet<SystemSetting> SystemSettings => Set<SystemSetting>();
+    public DbSet<DeviceEnrollmentAuthorization> DeviceEnrollmentAuthorizations => Set<DeviceEnrollmentAuthorization>();
+    public DbSet<WebAuthnFlowState> WebAuthnFlowStates => Set<WebAuthnFlowState>();
+    public DbSet<AttendanceAuthorization> AttendanceAuthorizations => Set<AttendanceAuthorization>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -88,6 +93,58 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
             entity.HasIndex(x => x.Accepted);
             entity.HasOne(x => x.Employee).WithMany(x => x.AttendanceAttempts).HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.SetNull);
             entity.HasOne(x => x.WorkSite).WithMany(x => x.AttendanceAttempts).HasForeignKey(x => x.WorkSiteId).OnDelete(DeleteBehavior.SetNull);
+        });
+
+        builder.Entity<EmployeeWebAuthnCredential>(entity =>
+        {
+            entity.ToTable("employee_webauthn_credentials");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.CredentialId).IsRequired();
+            entity.Property(x => x.PublicKey).IsRequired();
+            entity.Property(x => x.UserHandle).IsRequired();
+            entity.Property(x => x.CredentialType).HasMaxLength(50).IsRequired();
+            entity.HasIndex(x => x.CredentialId).IsUnique();
+            entity.HasIndex(x => x.EmployeeId).IsUnique().HasFilter("\"IsActive\" = TRUE").HasDatabaseName("ux_employee_active_webauthn_credential");
+            entity.HasOne(x => x.Employee).WithMany().HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        builder.Entity<SystemSetting>(entity =>
+        {
+            entity.ToTable("system_settings");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.RequireWorkerDeviceVerification).HasDefaultValue(false);
+            entity.HasData(new SystemSetting { Id = 1, RequireWorkerDeviceVerification = false, UpdatedAt = new DateTime(2026, 8, 29, 0, 0, 0, DateTimeKind.Utc) });
+        });
+
+        builder.Entity<DeviceEnrollmentAuthorization>(entity =>
+        {
+            entity.ToTable("device_enrollment_authorizations");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.TokenHash).HasMaxLength(64).IsRequired();
+            entity.HasIndex(x => x.TokenHash).IsUnique();
+            entity.HasIndex(x => new { x.EmployeeId, x.ExpiresAtUtc });
+            entity.HasOne(x => x.Employee).WithMany().HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        builder.Entity<WebAuthnFlowState>(entity =>
+        {
+            entity.ToTable("webauthn_flow_states");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Purpose).HasMaxLength(50).IsRequired();
+            entity.Property(x => x.OptionsJson).HasColumnType("text").IsRequired();
+            entity.HasIndex(x => new { x.EmployeeId, x.ExpiresAtUtc });
+            entity.HasOne(x => x.Employee).WithMany().HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        builder.Entity<AttendanceAuthorization>(entity =>
+        {
+            entity.ToTable("attendance_authorizations");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.AttemptType).HasConversion<int>();
+            entity.Property(x => x.TokenHash).HasMaxLength(64).IsRequired();
+            entity.HasIndex(x => x.TokenHash).IsUnique();
+            entity.HasIndex(x => new { x.EmployeeId, x.ExpiresAtUtc });
+            entity.HasOne(x => x.Employee).WithMany().HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
